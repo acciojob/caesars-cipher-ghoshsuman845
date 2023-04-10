e27b312d9089da61b01ae2330c4a6a858e86baf5
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-04-10 15:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-// Your Script here.
-
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
-
 function rot13(str) {
-  let decoded = "";
-
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-
-    if (char.match(/[A-Z]/)) {
-      let code = str.charCodeAt(i) - 13;
-      if (code < 65) {
-        code += 26;
-      }
-      char = String.fromCharCode(code);
-    }
-
-    decoded += char;
-  }
-
-  return decoded;
+  var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
 }
-
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
