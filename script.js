6b9ae9a7c4059675f0802e58b73bf68cefd32f5a
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-04-13 17:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function rot13(str) {
-  var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-  var index = x => input.indexOf(x);
-  var translate = x => index(x) > -1 ? output[index(x)] : x;
-  return str.split('').map(translate).join('');
+  var result = "";
+  for (var i = 0; i < str.length; i++) {
+    var c = str.charCodeAt(i);
+    if (c >= 65 && c <= 90) {  // only operate on uppercase letters
+      c = ((c - 65 + 13) % 26) + 65;  // shift by 13 places
+    }
+    result += String.fromCharCode(c);
+  }
+  return result;
 }
